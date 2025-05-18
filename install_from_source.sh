926244b1435da8f2a8e4fbf6786459b4ea8ff6de
PeterC <petercalifano.gs@gmail.com>
2025-05-18 22:59:07 +0200

    Complete installation script

--- install_from_source.sh
+++ install_from_source.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 # Script install moveit2 from source. It assumes Ubuntu 24.04 and ROS2 Jazzy Jalisco installed.
+# Reference documentation: https://moveit.picknik.ai/main/doc/tutorials/getting_started/getting_started.html
+
+moveit2_install_dir=~/devDir/ws_ros/ws_moveit
 
 # Prepare ROS2 environment
 source /opt/ros/jazzy/setup.bash
@@ -19,7 +22,7 @@ colcon mixin update default
 sudo apt install python3-vcstool
 
 # Create test workspace
-mkdir -p ~/devDir/ws_ros/ws_moveit
+mkdir -p $moveit2_install_dir
 
 # Remove any other installation
 sudo apt remove ros-$ROS_DISTRO-moveit*
@@ -28,5 +31,14 @@ sudo apt remove ros-$ROS_DISTRO-moveit*
 sudo apt update && rosdep install -r --from-paths . --ignore-src --rosdistro $ROS_DISTRO -y
 
 # Configure colcon worspace
-cd ~/devDir/ws_ros/ws_moveit
+cd $moveit2_install_dir
 colcon build --mixin release
+
+# NOTE on the command above, from doc:
+# Some of the packages built with this command require up to 16Gb of RAM to build. By default, colcon tries to build as many packages as possible at the same time. If you are low on computer memory, or if the build is generally having trouble completing on your computer, you can try appending --executor sequential to the colcon command above to build only one package at a time, or --parallel-workers <X> to limit the number of simultaneous builds. For even more limited machines, you can try running MAKEFLAGS="-j4 -l1" colcon build --executor sequential.
+
+# Source colcon workspace for moveit2
+source $moveit2_install_dir/install/setup.bash
+
+# To add to bashrc:
+#echo 'source $moveit2_install_dir/install/setup.bash' >> ~/.bashrc
